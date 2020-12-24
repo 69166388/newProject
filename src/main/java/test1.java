e02f2f4fb3509c93af1bcaedb2e65e5c8d3d45a4
dangxinyu1 <dangxinyu1@sensetime.com>
2020-12-24 21:25:07 +0800

    测试

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -1,21 +1,9 @@
 public class test1 {
 
     public static void main(String[] args) {
-        System.out.printf("1");
-        System.out.printf("2");
-        System.out.printf("3");
-        System.out.printf("4");
-        System.out.printf("5");
-        System.out.printf("6");
 
 
 
-        System.out.printf("7");
-        System.out.printf("8");
-
-        System.out.printf("9");
-        System.out.printf("10");
-        System.out.printf("11");
 
     }
 }
