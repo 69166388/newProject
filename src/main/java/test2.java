5059a19fb06f17bed169007095b69a98a46a32be
dangxinyu1 <dangxinyu1@sensetime.com>
2020-12-24 21:19:54 +0800

    删除代码

--- src/main/java/test2.java
+++ src/main/java/test2.java
@@ -1,13 +1,7 @@
 public class test2 {
 
     public static void main(String[] args) {
-        System.out.printf("1");
-        System.out.printf("2");
-        System.out.printf("3");
-        System.out.printf("4");
-        System.out.printf("5");
-        System.out.printf("6");
-        System.out.printf("7");
+
 
     }
 }
