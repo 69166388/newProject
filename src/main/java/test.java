b7702cee1e7adb568828a0fbfb835c6ed4c1a606
chenchen <2391848749@.com>
2020-12-27 19:15:57 +0800

    modified test.java

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,21 +1,6 @@
 public class test {
     public static void main(String[] args) {
-        System.out.printf("pre1");
-        System.out.printf("1");
-        System.out.printf("2");
-        System.out.printf("3");
-
-        System.out.printf("2");
-        System.out.printf("3");
-
-        System.out.printf("3");
-
-
-
-        System.out.printf("pre1");
-        System.out.printf("4");
-        System.out.printf("5");
-        System.out.printf("6");
+        System.out.println("第一次修改！！！");
 
 
     }
